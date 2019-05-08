08b42c33852e211a923454ae42e5b3360219441e
Marie Findlay <mariealicefindlay@gmail.com>
2019-05-08 22:02:29 +0100

    Add basket reducers

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -2,7 +2,43 @@ import { combineReducers } from "redux";
 
 import { actionTypes } from "../actions/actions";
 
-const currentPage = (state = 0, action) => {
+const initialState = {
+  currentPage: 0,
+  basket: {
+    itemQuantitiesById: {},
+  }
+}
+
+const basket = (state = initialState.basket, action) => {
+  switch (action.type){
+    case actionTypes.ADD_ITEMS_TO_BASKET:
+      let newObj = state.itemQuantitiesById;
+      action.itemIds.forEach(itemId => {
+        newObj[itemId] = 1
+      })
+      return {
+        itemQuantitiesById: newObj,
+      }
+    case actionTypes.INCREASE_QUANTITY:
+      return {
+        itemQuantitiesById: {
+          ...state.itemQuantitiesById,
+        [action.itemId]: state.itemQuantitiesById[action.itemId] + 1
+        },
+      }
+    case actionTypes.DECREASE_QUANTITY:
+      return {
+        itemQuantitiesById: {
+          ...state.itemQuantitiesById,
+        [action.itemId]: state.itemQuantitiesById[action.itemId] - 1
+        },
+      }
+    default:
+      return state;
+  }
+}
+
+const currentPage = (state = initialState.currentPage, action) => {
   switch (action.type) {
     case actionTypes.GO_TO_NEXT_PAGE:
       return state + 1;
@@ -15,6 +51,7 @@ const currentPage = (state = 0, action) => {
 
 const rootReducer = combineReducers({
   currentPage,
+  basket
 });
 
 export default rootReducer;
